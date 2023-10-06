434d29fc127e76de4546d342696cda76d539e358
Geekelo <efuelight12@gmail.com>
2023-10-06 11:44:40 +0100

    create model mothods to save and update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  # Custom Method: Update the comments counter for a post
+  after_save :update_post_comment_counter
+
+  def update_post_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_save :update_post_counter
+  # Custom Method: Update the posts counter for a user
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 end
