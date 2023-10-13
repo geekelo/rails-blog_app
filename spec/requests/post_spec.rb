cffcfdb9ffca29c9eb69e47e0b99c069d81783db
Geekelo <efuelight12@gmail.com>
2023-10-13 10:10:51 +0100

    Make required changes: Add tests for post show action

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -1,22 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :request do
-  let(:user) { User.create(name: 'John Doe') }
+RSpec.describe 'Posts', type: :request do
+  let(:user) { User.create(name: 'test user') }
+  let(:post) { user.posts.create(title: 'Test Post') }
 
-  describe 'GET /users/:user_id/posts' do
-    it 'returns a successful response' do
-      get user_posts_path(user)
+  describe 'GET /index' do
+    before(:example) { get user_posts_path(user) }
+
+    it 'displays a list of posts' do
       expect(response).to have_http_status(:success)
     end
 
-    it 'renders the index template' do
-      get user_posts_path(user)
-      expect(response).to render_template(:index)
+    it 'renders the correct template' do
+      expect(response).to render_template('index')
     end
 
     it 'includes correct placeholder text in the response body' do
-      get user_posts_path(user)
       expect(response.body).to include('post index')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get user_post_path(user, post) }
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('post show')
     end
   end
 end
