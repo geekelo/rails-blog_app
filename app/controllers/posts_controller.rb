2fc1dd9be7b270287b888dd4a028545bd6430288
geekelo <efueliht12@gmail.com>
2023-10-16 09:19:13 +0100

    Add views layouts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def index
     # Placeholder for all post page
+    @user = User.find(params[:user_id])  # Assuming you're using user_id from params
+    @posts = @user.posts
   end
 
   def show
     # Placeholder for single post page
+    @post = Post.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
     # Placeholder for all users page
+    @users = User.all
   end
 
   def show
     # Placeholder for single user page
+    @user = User.find(params[:id])
   end
 end
