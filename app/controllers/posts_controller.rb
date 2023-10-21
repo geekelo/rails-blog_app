78c8824131b86719d0edc8cf1dfe3d13021fdc67
geekelo <efueliht12@gmail.com>
2023-10-21 19:03:52 +0100

    Add form to create post, comment and like

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,29 @@ class PostsController < ApplicationController
     # Placeholder for single post page
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = Post.new(post_parameters)
+    @post.author_id = @user.id
+
+    if @post.save
+      flash[:notice] = 'The post is successfully submitted'
+      redirect_to user_post_path(@user, @post)
+    else
+      flash[:alert] = 'Can not save the post'
+      render :new
+    end
+  end
+
+  private
+
+  def post_parameters
+    params.require(:post).permit(:title, :text)
+  end
 end
