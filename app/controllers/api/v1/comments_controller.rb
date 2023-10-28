5878328672609fba8bb96b236a689c1b8a27eda2
geekelo <efueliht12@gmail.com>
2023-10-28 19:49:17 +0100

    Add API endpoint

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::CommentsController < ApplicationController
   before_action :find_post
 
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::PostsController < ApplicationController
   before_action :find_user
 
